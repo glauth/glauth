02970c82ff0b1ae583803e966414babc25c9190d
Ben Yanke <ben@benyanke.com>
2018-05-12 19:32:59 -0500

    Testing switching travis to make

--- scripts/travis/integration-test.sh
+++ scripts/travis/integration-test.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
+
+# Ensure ldap utils are installed (for example - when running this outside of travis)
 if [[ `which ldapsearch` ]]; then
 	echo "ldap-utils installed - continuing";
 else
 	sudo apt-get -qq update && sudo apt-get -qq install -y ldap-utils || exit 1;
 fi
 
-$TRAVIS_BUILD_DIR/glauth -c "$TRAVIS_BUILD_DIR/$1" &
+# Start in background, capture PID
+$TRAVIS_BUILD_DIR/bin/glauth64 -c "$TRAVIS_BUILD_DIR/$1" &
 glauthPid="$!"
 
 echo "Running at $glauthPid"
+
+
+# Run tests here
 sleep 2;
+
+# Kill saved PID when done
 kill $glauthPid;
 echo "done."
