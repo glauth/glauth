8c3fe078b234addcb768087ad1e859203becb90d
shipperizer <alexcabb@gmail.com>
2023-10-03 21:34:13 +0100

    fix: use newer function to fetch git info for version

--- v2/glauth.go
+++ v2/glauth.go
@@ -7,6 +7,7 @@ import (
 	"github.com/arl/statsviz"
 	docopt "github.com/docopt/docopt-go"
 	"github.com/fsnotify/fsnotify"
+	"github.com/glauth/glauth/v2/internal/version"
 	"github.com/glauth/glauth/v2/pkg/config"
 	"github.com/glauth/glauth/v2/pkg/frontend"
 	"github.com/glauth/glauth/v2/pkg/logging"
@@ -24,14 +25,6 @@ import (
 	"strings"
 	"time"
 )
-
-// Set with buildtime vars
-var LastGitTag string
-var BuildTime string
-var GitCommit string
-var GitClean string
-var GitBranch string
-var GitTagIsCommit string
 
 const programName = "glauth"
 
@@ -64,48 +57,6 @@ var (
 
 	activeConfig = &config.Config{}
 )
-
-// Reads builtime vars and returns a full string containing info about
-// the currently running version of the software. Primarily used by the
-// --version flag at runtime.
-func getVersionString() string {
-
-	var versionstr string
-
-	versionstr = "GLauth"
-
-	// Notate the git context of the build
-	switch {
-	// If a release, use the tag
-	case GitClean == "1" && GitTagIsCommit == "1":
-		versionstr += " " + LastGitTag + "\n\n"
-
-	// If this branch had a tag before, mention the branch and the tag to give a rough idea of the base version
-	case len(GitBranch) > 1 && len(LastGitTag) > 1:
-		versionstr += "\nNon-release build from branch " + GitBranch + ", based on tag " + LastGitTag + "\n\n"
-
-	// If no previous tag specified, just mention the branch
-	case len(GitBranch) > 1:
-		versionstr += "\nNon-release build from branch " + GitBranch + "\n\n"
-
-	// Fallback message, if all else fails
-	default:
-		versionstr += "\nNon-release build\n\n"
-	}
-
-	// Include build time
-	if len(BuildTime) > 1 {
-		versionstr += "Build time: " + BuildTime + "\n"
-	}
-
-	// Add commit hash
-	if GitClean == "1" && len(GitCommit) > 1 {
-		versionstr += "Commit: " + GitCommit + "\n"
-	}
-
-	return versionstr
-
-}
 
 func main() {
 	if err := parseArgs(); err != nil {
@@ -250,7 +201,7 @@ func startConfigWatcher() {
 func parseArgs() error {
 	var err error
 
-	if args, err = docopt.Parse(usage, nil, true, getVersionString(), false); err != nil {
+	if args, err = docopt.Parse(usage, nil, true, version.GetVersion(), false); err != nil {
 		return err
 	}
 
